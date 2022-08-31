2060d937184837529483894519e4dd414dc3593e
Mauroskyer <57736533+Mauroskyer@users.noreply.github.com>
2022-08-31 17:44:43 -0500

    First Commit

--- Drum Kit/keyboard-sounds.js
+++ Drum Kit/keyboard-sounds.js
@@ -5,14 +5,14 @@
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;// stop the fucntion from running all together
+    if (!audio) return;//stop the fucntion from running all together
     key.classList.add('playing');
-    audio.currentTime = 0;// rewind to the start
+    audio.currentTime = 0;//rewind to the start
     audio.play();
 }
 
 function removeTransition(e) {
-    if (e.propertyName !== 'transform') return; // skip it it´s not a transform
+    if (e.propertyName !== 'transform') return; // skip it it´s not a transfo
     e.target.classList.remove('playing');
 }
 
